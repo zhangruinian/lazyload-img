1ed023f638fc62fad29176708281ff0b9481dc0c
“frontIT” <zhangruinian3@gami.com>
2018-05-28 17:46:01 +0800

    增加update destory方法

--- src/index.js
+++ src/index.js
@@ -1,53 +1,78 @@
 class LazyLoad {
     constructor(options = {}) {
         this.options = {
-            root: null, // 根元素 必须是父元素
+            dataSrc: 'src',
+            root: null, // 根元素 必须是父元素 document时可改为 null
             rootMargin: 0, // 间距,多少px之前就开始调用
             threshold: [0] // 调用次数 0-1
         }
         this.io = null
+        this._elements = []
         Object.assign(this.options, options)
         this._init()
     }
 
     _init() {
-        var self = this
         console.log(this.options)
+
+        let {dataSrc} = this.options
+        let root = this.options.root || document
+
+        this._setObserver()
+        this._elements = Array.from(root.querySelectorAll(`img[data-${dataSrc}]`))
+        this._checkImgs()
+    }
+
+    _setObserver() {
+        if(!('IntersectionObserver' in window)){
+            return console.warn('当前浏览器不支持IntersectionObserver或没有加入相应polyfill ')
+        }
+
         this.io = new IntersectionObserver(entries => {
             console.log(entries)
             for (const entry of entries) {
                 const el = entry.target
                 const intersectionRatio = entry.intersectionRatio
                 if (intersectionRatio > 0 && intersectionRatio <= 1) {
-                    this._loadImg(el)
                     console.log(`${el.dataset.src}进入可视区..`)
+                    this._loadImg(el)
                 }
-                // 加载完成之后 不再observer
-                el.onload = el.onerror = function () {
-                    console.log(`${this.dataset.src}图片加载完成`)
-                    self.io.unobserve(el)
-                }
+
             }
 
         }, this.options)
-
-        this._checkImgs()
     }
 
     _loadImg(el) {
+        let self = this
         const source = el.dataset.src
         el.src = source
         // el.removeAttribute('data-src')
+        // 加载完成之后 不再observer
+        el.onload = el.onerror = function () {
+            console.log(`${this.dataset.src}图片加载完成`)
+            self.io.unobserve(el)
+        }
     }
 
     _checkImgs() {
-        // ${this.options.root}
-        const imgs = Array.from(document.querySelectorAll(`img[data-src]`)
-        )
-        imgs.forEach(item => this.io.observe(item))
+        if(!this._io){
+            this.update()
+        }
+        this._elements.forEach(item => this.io.observe(item))
     }
 
-    _destroy() {
+    update() {
+        // fallback处理
+        if(this.io){
+            return
+        }
+        this._elements.forEach((el) =>{
+            this._loadImg(el)
+        })
+    }
+
+    destroy() {
         this.io.disconnect()
         this.io = null
         this.options = null
